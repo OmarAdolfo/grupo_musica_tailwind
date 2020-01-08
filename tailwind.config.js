ae6ae0235cb937f4db34c390c8cd90ae9cdb1ff3
oalvher <oalvher95@gmail.com>
2020-01-08 18:07:10 +0000

    PEC3
    > Se recrean las páginas de miembros de la banda y contacto con tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      body: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        gray: '#344955',
+        yellow: '#f9aa33'
+      },
+      borderWidth: {
+        '7': '7px'
+      }
+    }
   },
   variants: {},
   plugins: []
